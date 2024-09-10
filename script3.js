4c80fa394d23e2cfbf39b0c5ab96ae4957aaca98
Daniil <morozkov.d.a@gmail.com>
2024-09-09 17:19:45 -0700

    backup before form added

--- script3.js
+++ script3.js
@@ -1,5 +1,5 @@
 const mobileMenu = document.getElementById('mobile-menu');
-const navList = document.querySelector('frame-1');
+const navList = document.querySelector('.frame-1');
 
 mobileMenu.addEventListener('click', () => {
     navList.classList.toggle('active');
