7ca79ae67a935bdb6a87bd464dd7ff206819b1c2
Daniil <morozkov.d.a@gmail.com>
2024-09-10 10:10:48 -0700

    backup before form added

--- script3.js
+++ script3.js
@@ -1,6 +1,18 @@
 const mobileMenu = document.getElementById('mobile-menu');
 const navList = document.querySelector('.frame-1');
+const navLinks = document.querySelectorAll('.frame-1 a');
+const closeMenu = document.getElementById('close-menu');
 
 mobileMenu.addEventListener('click', () => {
     navList.classList.toggle('active');
+});
+
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        navList.classList.remove('active');
+    });
+});
+
+closeMenu.addEventListener('click', () => {
+    navList.classList.remove('active');
 });
